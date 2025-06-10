6351f23174f708708c03ab3b2ff51e62629c39c7
Raidenknights <manas.vishnoi@gmail.com>
2025-06-11 00:46:30 +0530

    Adding repository

--- accountservice/src/main/java/accountservice/service/impl/AccountServiceImpl.java
+++ accountservice/src/main/java/accountservice/service/impl/AccountServiceImpl.java
@@ -1,24 +1,33 @@
 package accountservice.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import accountservice.dto.request.AccountDTO;
 import accountservice.entity.Account;
+import accountservice.repository.AccountRepository;
 import accountservice.service.AccountService;
 
 @Service
 public class AccountServiceImpl implements AccountService {
 
-	private final PasswordEncoder passwordEncoder;
+	private static final Logger logger = LoggerFactory.getLogger(AccountServiceImpl.class);
 
-	public AccountServiceImpl(PasswordEncoder passwordEncoder) {
+	private final PasswordEncoder passwordEncoder;
+	private final AccountRepository accountRepository;
+
+	public AccountServiceImpl(PasswordEncoder passwordEncoder, AccountRepository accountRepository) {
 		this.passwordEncoder = passwordEncoder;
+		this.accountRepository = accountRepository;
 	}
 
 	@Override
 	public void createAccount(AccountDTO accountDto, String uuid) {
+		logger.debug("enterting create account with uuid:"+uuid);
 		Account accountEntity = mapToEntityAccount(accountDto);
+		accountRepository.save(accountEntity);
 	}
 
 	@Override
